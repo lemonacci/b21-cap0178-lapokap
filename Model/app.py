4f66ce922559afc7329d84a8297bb7f1456b9057
fahriesa02 <fujinkai02@gmail.com>
2021-06-09 22:58:23 +0800

    added app.yaml

--- Model/app.py
+++ Model/app.py
@@ -9,13 +9,15 @@ app = Flask(__name__)
 
 @app.route('/api/', methods=['GET','POST'])
 def chatBot():
+    global chatInput
     if request.method == 'POST':
-        chatInput = (request.form.get('chatInput'))
+        chatInput = (request.get_json(force=True))
         print(chatInput)
-    chatBotReply = chat(chatInput)
+        global chatBotReply
+        chatBotReply = chat(chatInput)
     return jsonify(chatBotReply)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='5000')
+    app.run(debug=True, host='192.168.1.6', port='5000')
     modelfile = 'tokenizer.pickle'
     model = p.load(open(modelfile, 'rb'))
